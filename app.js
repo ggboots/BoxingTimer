21a1787065921b263b7e0d290e471d1da9568d3c
George <ggboots.99@gmail.com>
2021-11-02 20:46:52 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var button = document.createElement("button");
+button.setAttribute("class", "timerButton");
+var buttonContent = document.createTextNode("Start Timer");
+
+button.appendChild(buttonContent);
+
+document.body.appendChild(button);
