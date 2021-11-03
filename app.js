f717bcbcefb2a8bee676b8f87a62692d85662ace
George <ggboots.99@gmail.com>
2021-11-03 17:33:34 +1100

    setTimeout testing

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+var appStart = false;
+
+
 var button = document.createElement("button");
 button.setAttribute("id", "timerButton");
 var buttonContent = document.createTextNode("Start Timer");
-
 button.appendChild(buttonContent);
-
 document.body.appendChild(button);
+        
 
 
 button.addEventListener("click", button => {
-    var flashCard = document.createElement("div");
-    flashCard.setAttribute("id", "flashCard1");
-    // document.getElementById('flashCard1').style.height ='100px';
-    document.body.appendChild(flashCard);
+    document.getElementById("timerButton").remove();
+
+    setTimeout(createDiv, 1000);
+
+    function createDiv(){
+        
+        var flashCard = document.createElement("div");
+        flashCard.setAttribute("id", "flashCard1");
+        // document.getElementById('flashCard1').style.height ='100px';
+        document.body.appendChild(flashCard);
+    }
 })
