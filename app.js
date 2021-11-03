e02eb3f9f90e8e96eb42901e951866035fd453ea
George <ggboots.99@gmail.com>
2021-11-03 11:07:25 +1100

    basic functionality implemented

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var button = document.createElement("button");
-button.setAttribute("class", "timerButton");
+button.setAttribute("id", "timerButton");
 var buttonContent = document.createTextNode("Start Timer");
 
 button.appendChild(buttonContent);
 
 document.body.appendChild(button);
+
+
+button.addEventListener("click", button => {
+    var flashCard = document.createElement("div");
+    flashCard.setAttribute("id", "flashCard1");
+    // document.getElementById('flashCard1').style.height ='100px';
+    document.body.appendChild(flashCard);
+})
