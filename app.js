dd5e685b122ddc92df311bcb56b331c35b3a9d58
George Boots <ggboots.99@gmail.com>
2021-12-25 13:18:11 +1100

    morning session

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var timeMinutes = 0;
-var timeSeconds = 5;
+var timeMinutes = 3;
+var timeSeconds = 0;
 var main = document.getElementById("main");
 var child = main.getElementsByTagName("div");
+var switchToRandom = 2;
+var switchUnitOfTime = true;
+var selectDeSelectMinutes = true;
+var selectDeSelectSeconds = true;
 
 // Last task
 // Click on minutes or Seconds to change timer
-// Different modes, 
+// Different modes, (Jab+Jab(next move))
+// Countdown timer
+
+// Also how to access array of id's being created to save over use
 
 function start(){
     // creates front page element
@@ -29,9 +36,32 @@ function start(){
     timerElement4.setAttribute("id", "timerButtons");
     timerElement5.setAttribute("id", "timerButtonSelectUp");
     timerElement6.setAttribute("id", "timerButtonSelectDown");
+
     timerElement1.innerHTML = timeMinutes; 
+    timerElement1.style.cursor = "pointer";
+    timerElement1.addEventListener("click", function(){
+        if (selectDeSelectMinutes == true){
+            timerElement1.style.background = "#999999";
+            switchUnitOfTime = false;
+            selectDeSelectMinutes = false;
+        } else if (selectDeSelectMinutes == false){
+            timerElement1.style.background = "white";
+            switchUnitOfTime = true;
+            selectDeSelectMinutes = true;
+        }
+    });
     timerElement2.innerHTML = ":";
     timerElement3.innerHTML = timeSeconds.toLocaleString(undefined,{minimumIntegerDigits:2}); 
+    timerElement3.style.cursor = "pointer";
+    timerElement3.addEventListener("click", function(){
+        // if (selectDeSelectSeconds == true){
+        //     timerElement3.style.background = "#999999";
+        //     selectDeSelectSeconds = false;
+        // } else if (selectDeSelectSeconds == false){
+        //     timerElement3.style.background = "white";
+        //     selectDeSelectSeconds = true;
+        // }
+    });
     timerElement5.innerHTML = "^";
     timerElement6.innerHTML = "^";
     document.getElementById("timeSelection").appendChild(timerElement1);
@@ -40,29 +70,39 @@ function start(){
     document.getElementById("timeSelection").appendChild(timerElement4);
     document.getElementById("timerButtons").appendChild(timerElement5);
     document.getElementById("timerButtons").appendChild(timerElement6);
-   
+
     // Dynamically access created element
     var timerArrowUp = document.getElementById("timerButtonSelectUp");
     var timerArrowDown = document.getElementById("timerButtonSelectDown");
 
     timerArrowUp.addEventListener("click", function(){
-        timeSeconds++;
-        if (timeSeconds == 60){
-            timeMinutes++;
-            timeSeconds = 0;
-        }
-        timerElement3.innerHTML = timeSeconds;
-        timerElement1.innerHTML = timeMinutes;
+        if (switchUnitOfTime == true){
+            timeSeconds++;
+            if (timeSeconds == 60){
+                timeMinutes++;
+                timeSeconds = 0;
+            }
+            } else if (switchUnitOfTime == false){
+                timeMinutes++;
+            }
+            timerElement3.innerHTML = timeSeconds.toLocaleString(undefined,{minimumIntegerDigits:2});
+            timerElement1.innerHTML = timeMinutes;
+        
     });
 
     timerArrowDown.addEventListener("click", function(){
-        timeSeconds--;
-        if(timeSeconds == -1){
-            timeMinutes--;
-            timeSeconds = 59;
-        }
-        timerElement3.innerHTML = timeSeconds;
-        timerElement1.innerHTML = timeMinutes;
+        if (switchUnitOfTime == true){
+            timeSeconds--;
+            if(timeSeconds == -1){
+                timeMinutes--;
+                timeSeconds = 59;
+            }   
+            } else if (switchUnitOfTime == false){
+                timeMinutes--;
+            }
+            timerElement3.innerHTML = timeSeconds.toLocaleString(undefined,{minimumIntegerDigits:2});
+            timerElement1.innerHTML = timeMinutes;
+        
     });
 
     button.addEventListener("click", button => {
@@ -140,6 +180,35 @@ function start(){
         }
     }
 
+    function jabJabrandomFunc(){
+        if (time == 0){
+            clearInterval(flashCardPresent);
+            clearInterval(countDownTimer);
+            
+            console.log("Times up");
+            setTimeout(reloadProgram, 3000);
+        } 
+        
+        if (switchToRandom == 0){
+            var random = Math.floor(Math.random() * 3)+1;
+            switchToRandom = 2;
+            switch(random){
+                case 1:
+                    createUpperCut();
+                    break;
+                case 2:
+                    createCross();
+                    break;
+                case 3:
+                    createHook();
+                    break;
+            } 
+        } else {
+            switchToRandom--;
+            createJab();
+        }
+    }
+
     let time = (timeMinutes * 60) + timeSeconds;
 
     function countDownTimer(){
@@ -152,6 +221,7 @@ function start(){
     }
 
     var flashCardPresent = setInterval(randomFunc, 2000); 
+    // var flashCardJabJab = setInterval(jabJabRandomFunc, 2000);
     var countDownTimer = setInterval(countDownTimer, 1000);
 
 
